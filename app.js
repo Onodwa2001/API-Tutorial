31a237f8b30d2f349d12a8261776e3c840a87743
Onodwa2001 <onodwasiyotula7@gmail.com>
2022-08-03 12:27:02 +0200

    part 2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let input = document.getElementById('search-input');
+let cardContainer = document.getElementById('card-section')
+let loading = document.getElementById('loading');
 
 const options = {
 	method: 'GET',
@@ -11,11 +13,33 @@ const options = {
 document.getElementById('search').addEventListener('click', (e) => {
     e.preventDefault();
     let endpoint = `https://shazam.p.rapidapi.com/search?term=${input.value}&locale=en-US&offset=0&limit=5`;
+    loading.style.display = "block";
 
     fetch(endpoint, options)
         .then(response => response.json())
         .then(data => {
-            console.log(data.artists.hits)
+            let hitsArr = data.artists.hits
+
+            cardContainer.innerHTML = '';
+
+            hitsArr.forEach(element => {
+                console.log(element.artist)
+
+                cardContainer.innerHTML += `
+                    <div class="card">
+                        <div class="img">
+                            <img height="200px" width="200px" src="${element.artist.avatar}" alt="">
+                        </div>
+                        <div class="description">
+                            <h3>${element.artist.name}</h3>
+                            <a href="${element.artist.weburl}" target="_blank">Link to the song</a>
+                        </div>
+                    </div>
+                `;
+            });
+
+            loading.style.display = "none";
+
         })
         .catch(err => console.log(err))
 })
